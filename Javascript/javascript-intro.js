ec57c137b4732870aa855d3ad2f90d7bc08f9819
Sanket <sanketbhosale.codekul@gmail.com>
2021-10-05 12:05:36 +0530

    Javascript Operators

---
+++ Javascript/javascript-intro.js
@@ -0,0 +1,131 @@
+document.write("Hello, welcome to javscript")
+
+
+// var keyword variable is a global scope as well as function scope variable;
+// let keyword variable is block scope and function scope variable 
+
+
+c=55
+
+
+num2=555;
+console.log(num2);
+
+
+var num2;
+
+console.log(typeof(c))
+
+
+
+let a;
+
+a = [0,"Java",['a','b','c'],{name: "sanket", city: "pune"}];
+
+
+let car = {
+brand: "mercedez",
+color: "red",
+cost: 40000,
+array: [0,1,2,3,4]
+}
+
+
+console.log(car)
+
+
+// function add() {
+//     let a = 20;
+//     let b = 45;
+
+//     console.log(a+b)
+
+// }
+
+// add()
+
+function add (){
+    let a = 30;
+    let b = 45;
+
+    console.log(a+b)
+    console.log("hiiiiii")
+    return true;
+
+   
+
+}
+
+
+
+
+
+function showData(){
+    //console.log(data)
+   
+}
+
+
+
+
+showData()
+
+add()
+
+console.log(add())
+
+
+
+
+var num1 = 10;
+num1 = 45;
+
+console.log("num1",num1)
+
+
+
+const PI = 3.14;
+
+PI= 4000;
+
+console.log("PI", PI)
+
+
+
+
+
+document.write(a)
+
+console.log(a)
+
+
+if (true) {
+    let c ="hello, bhumit" ;
+
+    document.write(c)
+
+
+    console.log(c)
+   
+}
+
+
+
+
+
+
+
+
+function display() {
+    var b;
+
+b = 20;
+
+   
+}
+
+
+document.write(PI)
+
+
+display()
